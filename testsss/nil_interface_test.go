4a1ba178e0fab48384ebef3ec433920658648e4a
admin <admin@admindeMacBook-Pro.local>
2022-02-18 15:20:46 +0800

    😀🌴⭐🌎☀️🌈🌽: interface == nil ?

---
+++ testsss/nil_interface_test.go
@@ -0,0 +1,54 @@
+package testsss
+
+import "testing"
+
+type IF interface {
+	Foo()
+}
+
+type S struct {}
+func (s S) Foo() {}
+
+
+func TypeIsNil() S {
+	return S{}
+}
+
+func PointerIsNil() *S {
+	var s *S
+	return s
+}
+
+func EmptyInterfaceIsNil() interface{} {
+	var s S
+	return s
+}
+
+func EmptyInterfacePointerIsNil() interface{} {
+	var s *S
+	return s
+}
+
+func InterfaceIsNil() IF {
+	var s S
+	return s
+}
+
+func InterfacePointerIsNil() IF {
+	var s *S
+	return s
+}
+
+func TestNilInterface(t *testing.T) {
+	// t.Log(TypeIsNil() == nil)
+
+	t.Log(PointerIsNil() == nil)
+
+	// interface{} is "interface{}" not nil
+	t.Log(EmptyInterfaceIsNil() == nil)
+	t.Log(EmptyInterfacePointerIsNil() == nil)
+
+	// interface is "interface" not nil
+	t.Log(InterfaceIsNil() == nil)
+	t.Log(InterfacePointerIsNil() == nil)
+}
