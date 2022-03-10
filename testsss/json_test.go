37e1de2b25b7184bdd06029646f16f5848fb6f9a
GodYao <liyaoo1995@gmail.com>
2022-03-10 17:08:24 +0800

    😀🌴⭐🌎☀️🌈🌽: json-update

--- testsss/json_test.go
+++ testsss/json_test.go
@@ -6,18 +6,20 @@ import (
 )
 
 type TestJsonSuite struct {
-	Code string      `json:"code"`
-	Data interface{} `json:"data"`
-	Msg  string      `json:"msg"`
-	Tips string      `json:"tips,omitempty"`
+	Code   string      `json:"code"`
+	Data   interface{} `json:"data"`
+	Msg    string      `json:"msg"`
+	Tips   string      `json:"tips,omitempty"`
+	Secret string      `json:"-"`
 }
 
 func TestJson(t *testing.T) {
 	data := TestJsonSuite{
 		Code: "1",
 		Data: "data",
-		Msg: "msg",
-		Tips: "tips",
+		Msg:  "msg",
+		Tips: "",			//  omitempty zero value omit
+		Secret: "secret",   // `json:"-"` dont output
 	}
 
 	// bs, e := json.Marshal(data)
