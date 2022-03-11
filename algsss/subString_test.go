4aa739cff124911e482240426b516b1cdf10447d
GodYao <liyaoo1995@gmail.com>
2022-03-11 14:05:50 +0800

    😀🌴⭐🌎☀️🌈🌽: algs substring

---
+++ algsss/subString_test.go
@@ -0,0 +1,24 @@
+package algsss
+
+import (
+	"testing"
+)
+
+func FirstSubStringIndex(source, target string) (i int) {
+	if source == "" || target == "" {
+		return -1
+	}
+	for i = 0; i < len(source)-len(target)+1; i++ {
+		for j := 0; j < len(target); j++ {
+			if source[i+j] == target[j] {
+				return i
+			}
+		}
+	}
+	return -1
+}
+
+func TestSubString(t *testing.T) {
+	index := FirstSubStringIndex("abcdefghij", "cde")
+	t.Log(index)
+}
