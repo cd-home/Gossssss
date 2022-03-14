c7702081c1a8830f2bd20474950597176358f86f
GodYao <liyaoo1995@gmail.com>
2022-03-14 09:38:24 +0800

    😀🌴⭐🌎☀️🌈🌽: string to byte

--- testsss/bytes_test.go
+++ testsss/bytes_test.go
@@ -16,6 +16,18 @@ func TestBytes(t *testing.T) {
 
 	b[1] = '0'
 
-	t.Log(*ps) // 12345
+	t.Log(*ps)       // 12345
 	t.Log(string(b)) // 103
+}
+func TestStringSlice(t *testing.T) {
+	s := "123"
+	b := s[0]
+
+	// b is a byte 
+	t.Logf("%v, %v, %T", b, string(b), b) // 49, 1, uint8
+
+	// v is a byte 
+	for _, v := range s {
+		t.Logf("%v, %v, %T", v, string(b), v) // 49, 1, int32   50, 1, int32   51, 1, int32
+	}
 }
