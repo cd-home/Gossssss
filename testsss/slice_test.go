005b5e3004eb5e04550285caa3659ec565977261
GodYao <liyaoo1995@gmail.com>
2022-03-15 14:50:01 +0800

    😀🌴⭐🌎☀️🌈🌽: slice append

--- testsss/slice_test.go
+++ testsss/slice_test.go
@@ -92,3 +92,10 @@ func TestCopySlices(t *testing.T) {
 	t.Log(_copySlice)
 	t.Log(sliceTest)
 }
+
+func TestSliceAppend(t *testing.T) {
+	s1 := []int{1, 2, 3}
+	s2 := []int{4, 5, 6}
+	s1  = append(s1, s2...)
+	t.Log(s1)
+}
