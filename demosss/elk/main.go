486ee3039c25384532fd80d753853265b6c8741d
GodYao <liyaoo1995@gmail.com>
2022-03-15 17:29:29 +0800

    😀🌴⭐🌎☀️🌈🌽: demosss elastic

---
+++ demosss/elk/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/olivere/elastic/v7"
+	"reflect"
+)
+
+func main() {
+	elasticUrl := "http://127.0.0.1:9200"
+	client, err := elastic.NewClient(elastic.SetURL(elasticUrl), elastic.SetSniff(false))
+	if err != nil {
+		panic(err)
+	}
+	_, _, err = client.Ping(elasticUrl).Do(context.Background())
+	if err != nil {
+		panic(err)
+	}
+	exists, _ := client.IndexExists("db").Do(context.Background())
+	if exists {
+		type BookStore struct {
+			Author string `json:"author"`
+			Book   string `json:"book"`
+		}
+		// POST
+		//bs := BookStore{
+		//	Author: "李莫",
+		//	Book:   "AK47",
+		//}
+		//_, err = client.Index().Index("db").BodyJson(bs).Do(context.Background())
+		//fmt.Println(err)
+		_, _ = client.Search().Index("db").Do(context.Background())
+		//matchQuery := elastic.NewMatchPhrasePrefixQuery("author", "Ma")
+		// 只能小写
+		matchQuery := elastic.NewTermQuery("author", "JoMo")
+		matchQuery.CaseInsensitive(true)
+		searchResult, err := client.Search().Index("db").Query(matchQuery).Do(context.Background())
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println(searchResult.TotalHits())
+		for _, item := range searchResult.Each(reflect.TypeOf(BookStore{})) {
+			fmt.Println(item.(BookStore))
+		}
+	} else {
+		indicesCreateResult, err := client.CreateIndex("db").Do(context.Background())
+		if err != nil {
+			return
+		}
+		if !indicesCreateResult.Acknowledged {
+			fmt.Println("Not Created")
+			return
+		}
+	}
+}
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/go-redis/redis/v8 v8.11.4
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/gorilla/mux v1.8.0
+	github.com/olivere/elastic/v7 v7.0.31
 	github.com/stretchr/testify v1.7.0
 )
 
@@ -13,6 +14,9 @@ require (
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
 )
