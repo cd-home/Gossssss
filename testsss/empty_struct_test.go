a0ea8dc0311df5703f7b59e1840f26831248b437
GodYao <liyaoo1995@gmail.com>
2022-03-15 11:14:58 +0800

    😀🌴⭐🌎☀️🌈🌽: testsss empty struct compare and escape

---
+++ testsss/empty_struct_test.go
@@ -0,0 +1,30 @@
+package testsss
+
+import (
+	"testing"
+	"unsafe"
+)
+
+func TestCompareEmptyStruct(t *testing.T) {
+	s1 := new(struct {})
+	s2 := new(struct {})
+	
+	// 逃逸
+	t.Logf("%p", s1)
+	t.Logf("%p", s2)
+
+	t.Log(unsafe.Sizeof(s1))
+	t.Log(unsafe.Sizeof(s2))
+
+	t.Log(s1 == s2)
+
+	t.Log("=========================================")
+
+	// 没有逃逸
+	s3 := new(struct{})
+	s4 := new(struct{})
+
+	t.Log(unsafe.Sizeof(s3))
+	t.Log(unsafe.Sizeof(s4))
+	t.Log(s3 == s4)
+}
