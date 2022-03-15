481287349422ce343ba32699478a350be03df26c
GodYao <liyaoo1995@gmail.com>
2022-03-15 13:15:25 +0800

    😀🌴⭐🌎☀️🌈🌽: testsss defer call delay

--- testsss/defer_test.go
+++ testsss/defer_test.go
@@ -22,3 +22,28 @@ func TestDeferredPanic(t *testing.T) {
 	}()
 	panic("TestDeferredPanic")
 }
+
+type Key struct {
+	Value []int
+}
+
+func (k *Key) Add(v int) *Key { 
+	k.Value = append(k.Value, v)
+	return k
+}
+
+func DeferDelayCall() *Key {
+	k := &Key{}
+
+	// first call add(1) to simple method, defer k.Add(2)
+	defer k.Add(1).Add(2)
+
+	k.Add(3)
+
+	return k
+}
+
+func TestDeferDelayCall(t *testing.T) {
+	k := DeferDelayCall()
+	t.Log(k.Value)
+}	
