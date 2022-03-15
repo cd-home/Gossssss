7542edf33e5d130536b09b24e9849a2180e3fdb8
GodYao <liyaoo1995@gmail.com>
2022-03-15 10:41:32 +0800

    😀🌴⭐🌎☀️🌈🌽: testsss define new type or alias for base type

---
+++ testsss/define_type_test.go
@@ -0,0 +1,21 @@
+package testsss
+
+import (
+	"testing"
+)
+// define new type based on base type
+type Myint int
+
+// alias to base type
+type Aliaint = int
+
+func TestDefineMyType(t *testing.T) {
+	var num1 int = 123
+	// var num2 Myint = 123
+	var num3 Aliaint = 123
+
+	// t.Log(num1 == num2)
+	// t.Log(num2 == num3)
+
+	t.Log(num1 == num3)
+}
