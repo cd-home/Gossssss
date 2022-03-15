052d10b657a35a74729b68d70bcb742985440c17
GodYao <liyaoo1995@gmail.com>
2022-03-15 17:37:21 +0800

    😀🌴⭐🌎☀️🌈🌽: std http download file

---
+++ stdsss/http/assets.go
@@ -0,0 +1,12 @@
+package http
+
+import (
+	"net/http"
+)
+
+func FileServer() {
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.ListenAndServe(":8080", nil)
+}
