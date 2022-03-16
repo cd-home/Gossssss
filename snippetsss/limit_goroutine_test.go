a17bf1410df4569213f3f49f3d8f19737327f2bd
GodYao <liyaoo1995@gmail.com>
2022-03-16 16:18:27 +0800

    😀🌴⭐🌎☀️🌈🌽: limit goroutine

--- snippetsss/limit_goroutine_test.go
+++ snippetsss/limit_goroutine_test.go
@@ -1,19 +1,56 @@
 package snippetsss
 
 import (
-	"testing"
-	"runtime"
 	"fmt"
+	"runtime"
+	"sync"
+	"testing"
 	"time"
 )
 
-func doWork(limit chan struct{}, i int)  {
+func SendTask(buf chan int, task int, wg *sync.WaitGroup) {
+	wg.Add(1)
+	buf <- task
+}
+
+func Work(buf chan int, wg *sync.WaitGroup) {
+	for task := range buf {
+		fmt.Println(task, runtime.NumGoroutine())
+		wg.Done()
+	}
+}
+
+// TestWorkPoolGoroutines will be better than TestLimitGoroutines
+func TestWorkPoolGoroutines(t *testing.T) {
+	t.Helper()
+	// watch out: wg must be *sync.WaitGroup (pointer)
+	wg := &sync.WaitGroup{}
+
+	buf := make(chan int)
+
+	// Wokers: 消费
+	worker := 3
+	for i := 0; i < worker; i++ {
+		go Work(buf, wg)
+	}
+
+	// 生产
+	tasks := 100
+	for i := 0; i < tasks; i++ {
+		SendTask(buf, i, wg)
+	}
+
+	wg.Wait()
+}
+
+func doWork(limit chan struct{}, i int) {
 	fmt.Println(i, runtime.NumGoroutine())
 	time.Sleep(time.Second)
 	<-limit
 }
 
 func TestLimitGoroutines(t *testing.T) {
+	t.Helper()
 	limit := make(chan struct{}, 3)
 	for i := 0; i < 10; i++ {
 		limit <- struct{}{}
