91981087dd9128e1419a20df90710a54762f2c75
GodYao <liyaoo1995@gmail.com>
2022-03-18 13:42:57 +0800

    😀🌴⭐🌎☀️🌈🌽: offersss revser linklist

---
+++ offersss/reverse_list_test.go
@@ -0,0 +1,36 @@
+package offersss
+
+import "testing"
+
+/**
+ * Definition for singly-linked list
+ */
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func ReverseList(head *ListNode) *ListNode {
+	if head == nil {
+		return head
+	}
+	var pre *ListNode
+	cur := head
+	for cur != nil {
+		// keep nexts
+		nexts := cur.Next
+
+		// reverse
+		cur.Next = pre
+
+		// move pointer
+		pre = cur
+		cur = nexts
+	}
+	return pre
+}
+
+func TestReverseLinkList(t *testing.T) {
+	ReverseList(&ListNode{})
+}
