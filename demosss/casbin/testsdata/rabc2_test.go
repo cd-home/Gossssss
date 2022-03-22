0e3db5e5186db7fec6703f5355ae5e325a234150
GodYao <liyaoo1995@gmail.com>
2022-03-22 16:58:13 +0800

    😀🌴⭐🌎☀️🌈🌽: demosss casbin define keyFucn matcher

--- demosss/casbin/testsdata/rabc2_test.go
+++ demosss/casbin/testsdata/rabc2_test.go
@@ -1,20 +1,36 @@
 package testdata
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/casbin/casbin/v2"
 )
 
-func TestRabcCasbin(t *testing.T) {
-	e, err := casbin.NewEnforcer("../configs/rabc/casbin_rabc_rule.conf", "../configs/rabc/casbin_policy_rule.csv")
+func KeyMatch(key1, key2 string) bool {
+	if i := strings.Index(key2, "*"); i != -1 {
+		return key1[:i] == key2[:i]
+	}
+	return key1 == key2
+}
+func KeyMatchFunc(args ...interface{}) (interface{}, error) {
+	key1 := args[0].(string)
+	key2 := args[1].(string)
+	return (bool)(KeyMatch(key1, key2)), nil
+}
+
+func TestRabcCasbinDeFineKeyFunc(t *testing.T) {
+	e, err := casbin.NewEnforcer("../configs/rabc2/casbin_rabc_rule.conf", "../configs/rabc2/casbin_policy_rule.csv")
 	if err != nil {
 		panic(err)
 	}
 
+	// define a match rule for obj
+	e.AddFunction("KeyMatchFunc", KeyMatchFunc)
+
 	e.LoadPolicy()
 	// admin
-	sub, obj, act := "admin", "data/1", "read"
+	sub, obj, act := "admin", "data/*", "read"
 	ok, _ := e.Enforce(sub, obj, act)
 	t.Logf("sub = %s, obj = %s, act = %s, ok=%v", sub, obj, act, ok)
 
@@ -27,7 +43,7 @@ func TestRabcCasbin(t *testing.T) {
 	ok, _ = e.Enforce(sub, obj, act)
 	t.Logf("sub = %s, obj = %s, act = %s, ok=%v", sub, obj, act, ok)
 
-	// role bob ==> developer == > data/2 read
+	// role
 	sub, obj, act = "bob", "data/1", "read"
 	ok, _ = e.Enforce(sub, obj, act)
 	t.Logf("sub = %s, obj = %s, act = %s, ok=%v", sub, obj, act, ok)
