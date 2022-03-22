c018a7b06b12873608ade2b507490148412b6ce4
GodYao <liyaoo1995@gmail.com>
2022-03-22 17:18:22 +0800

    😀🌴⭐🌎☀️🌈🌽: demosss casbin 用户与资源 双角色

--- demosss/casbin/testsdata/rabc3_test.go
+++ demosss/casbin/testsdata/rabc3_test.go
@@ -5,40 +5,42 @@ import (
 
 	"github.com/casbin/casbin/v2"
 )
-// 用户角色
-func TestRabcCasbin(t *testing.T) {
-	e, err := casbin.NewEnforcer("../configs/rabc/casbin_rabc_rule.conf", "../configs/rabc/casbin_policy_rule.csv")
+// 用户角色 与 资源角色
+func TestMultiRabcCasbin(t *testing.T) {
+	e, err := casbin.NewEnforcer("../configs/rabc3/casbin_rabc_rule.conf", "../configs/rabc3/casbin_policy_rule.csv")
 	if err != nil {
 		panic(err)
 	}
 
 	e.LoadPolicy()
 
-	// root
-	sub, obj, act := "root", "data/1", "read"
+	// admin
+	sub, obj, act := "admin", "data/1", "read"
 	ok, _ := e.Enforce(sub, obj, act)
 	t.Logf("sub = %s, obj = %s, act = %s, ok=%v", sub, obj, act, ok)
 
-	// admin
-	sub, obj, act = "admin", "data/1", "read"
+	sub, obj, act = "admin", "data/1", "write"
 	ok, _ = e.Enforce(sub, obj, act)
 	t.Logf("sub = %s, obj = %s, act = %s, ok=%v", sub, obj, act, ok)
 
-	// users
-	sub, obj, act = "alice", "data/1", "read"
+	sub, obj, act = "admin", "data/2", "write"
 	ok, _ = e.Enforce(sub, obj, act)
 	t.Logf("sub = %s, obj = %s, act = %s, ok=%v", sub, obj, act, ok)
 
-	sub, obj, act = "alice", "data/1", "write"
+	sub, obj, act = "admin", "data/2", "write"
 	ok, _ = e.Enforce(sub, obj, act)
 	t.Logf("sub = %s, obj = %s, act = %s, ok=%v", sub, obj, act, ok)
 
-	// role bob ==> developer == > data/2 read
-	sub, obj, act = "bob", "data/1", "read"
+	// bob prod can not
+	sub, obj, act = "bob", "data/1", "write"
 	ok, _ = e.Enforce(sub, obj, act)
 	t.Logf("sub = %s, obj = %s, act = %s, ok=%v", sub, obj, act, ok)
-
+	// dev can 
 	sub, obj, act = "bob", "data/2", "read"
+	ok, _ = e.Enforce(sub, obj, act)
+	t.Logf("sub = %s, obj = %s, act = %s, ok=%v", sub, obj, act, ok)
+
+	sub, obj, act = "bob", "data/2", "write"
 	ok, _ = e.Enforce(sub, obj, act)
 	t.Logf("sub = %s, obj = %s, act = %s, ok=%v", sub, obj, act, ok)
 }
