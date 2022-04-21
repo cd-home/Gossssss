92eaa08562d9decc9b4c313113dab1eeda267b67
GodYao <liyaoo1995@gmail.com>
2022-04-21 15:55:42 +0800

    😀🌴⭐🌎☀️🌈🌽: ip string

---
+++ snippetsss/ip_test.go
@@ -0,0 +1,44 @@
+package snippetsss
+
+import (
+	"fmt"
+	"net"
+	"strconv"
+	"strings"
+	"testing"
+)
+
+func TestIp(t *testing.T) {
+	ip := net.ParseIP("192.168.1.20")
+	fmt.Println(ip.DefaultMask().String())
+
+	fmt.Println(StringIpToUint("192.168.1.20"))
+	ips := StringIpToUint("192.168.1.20")
+
+	fmt.Println(UintIpToString(ips))
+
+	fmt.Println(0b0011 | 0b1100)
+	fmt.Println(0b1111)
+}
+
+func StringIpToUint(ip string) (uip uint32) {
+	pos := 24
+	ips := strings.Split(ip, ".")
+	for _, item := range ips {
+		v, _ := strconv.Atoi(item)
+		v = v << pos
+		uip = uip | uint32(v)
+		pos -= 8
+	}
+	return
+}
+
+func UintIpToString(ip uint32) string {
+	ipUint := []uint32{ip >> 24 & 0xFF, ip >> 16 & 0xFF, ip >> 8 & 0xFF, ip & 0xFF}
+	var ips []string
+	for _, item := range ipUint {
+		s := strconv.Itoa(int(item))
+		ips = append(ips, s)
+	}
+	return strings.Join(ips, ".")
+}
