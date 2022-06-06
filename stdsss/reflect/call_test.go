a97ad19943ae63677afb9b3b9a7f80b33a8ce7e1
GodYao <liyaoo1995@gmail.com>
2022-06-06 13:35:32 +0800

    😀🌴⭐🌎☀️🌈🌽: Reflect Export Field And Func Call

--- stdsss/reflect/call_test.go
+++ stdsss/reflect/call_test.go
@@ -19,34 +19,51 @@ func (u User) SayHi(msg string) {
 	fmt.Printf("%s, I am %s\n", msg, u.Name)
 }
 
-func TestReflectCall(tt *testing.T) {
+func TestReflectCall(t *testing.T) {
 	user := User{Name: "GodYao", Age: 27}
 
-	object := reflect.ValueOf(&user)
+	rv := reflect.ValueOf(&user)
 	// By MethodName Call
-	f := object.MethodByName("SayHello")
+	f := rv.MethodByName("SayHello")
+
 	f.Call([]reflect.Value{})
 
 	// Got Fields
-	t := object.Elem()
-	for i := 0; i < t.NumField(); i++ {
-		field := t.Field(i)
+	ev := rv.Elem()
+	for i := 0; i < ev.NumField(); i++ {
+		field := ev.Field(i)
 		fmt.Printf("%d %s = %v \n", i, field.Type(), field.Interface())
 	}
 
 	// Call Methods By MethodName
 	Invoke(&user, "SayHello")
 	Invoke(&user, "SayHi", "Hi")
+
+	// tt.Log("line----")
+
+	// Func IsExported
+	rf := reflect.TypeOf(&user)
+	e := rf.Elem()
+	for i := 0; i < e.NumMethod(); i++ {
+		m := e.Method(i)
+		t.Log(m.Name)
+		if m.Name == "SayHello" && m.IsExported() {
+			// one way
+			rv.MethodByName(m.Name).Call([]reflect.Value{})
+			// another way, Func must use receiver be the first argument
+			m.Func.Call([]reflect.Value{reflect.ValueOf(user)})
+		}
+	}
 }
 
 func Invoke(o interface{}, funcName string, params ...interface{}) {
-	object := reflect.ValueOf(o)
+	rv := reflect.ValueOf(o)
 	ins := make([]reflect.Value, len(params))
 	// Real params
 	for i, value := range params {
 		ins[i] = reflect.ValueOf(value)
 	}
 	// Call Method
-	f := object.MethodByName(funcName)
+	f := rv.MethodByName(funcName)
 	f.Call(ins)
 }
--- stdsss/reflect/value_test.go
+++ stdsss/reflect/value_test.go
@@ -11,8 +11,29 @@ func TestTagValue(t *testing.T) {
 		Age  int
 	}
 	var u = User{}
-	vf := reflect.ValueOf(&u)
-	f := vf.Elem().FieldByName("Age")
+	rv := reflect.ValueOf(&u)
+	f := rv.Elem().FieldByName("Age")
 	f.SetInt(10)
 	t.Log(u)
+
+	for i := 0; i < rv.NumField(); i++ {
+		field := rv.Field(i)
+		if !field.CanSet() {
+			continue
+		}
+	}
+
+	// judge the field IsExported
+	rt := reflect.TypeOf(&u)
+	for i := 0; i < rt.NumField(); i++ {
+		field := rt.Field(i)
+		if !field.IsExported() {
+			continue
+		}
+		// Means that the IsExported
+		if len(field.PkgPath) == 0 {
+			continue
+		}
+	}
+
 }
