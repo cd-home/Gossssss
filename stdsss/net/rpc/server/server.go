c0029395751b277caf7bfb8be9db14c31777dcc8
GodYao <liyaoo1995@gmail.com>
2022-07-22 15:17:43 +0800

    😀🌴⭐🌎☀️🌈🌽: std net/rpc

---
+++ stdsss/net/rpc/server/server.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+	"net"
+	"net/rpc"
+)
+
+type Greeter struct{}
+
+// 严格符合这种格式的方法，就可以对外提供服务
+func (g *Greeter) Hello(req string, resp *string) error {
+	*resp = "Hello, " + req
+	return nil
+}
+
+func main() {
+	// 注册
+	_ = rpc.Register(&Greeter{})
+
+	// 监听地址，提供服务
+	addr, _ := net.ResolveTCPAddr("tcp", ":8081")
+	listener, _ := net.ListenTCP("tcp", addr)
+
+	for {
+		conn, e := listener.AcceptTCP()
+		if e != nil {
+			continue
+		}
+		log.Printf("%s\n", conn.RemoteAddr())
+		go rpc.ServeConn(conn)
+	}
+}
