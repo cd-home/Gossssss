9dbe00bec9e7efcfd9da37fb8c2369993f25eb0f
GodYao <liyaoo1995@gmail.com>
2022-07-22 14:07:36 +0800

    😀🌴⭐🌎☀️🌈🌽: std net/tcp

---
+++ stdsss/net/tcp/client/client.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"net"
+)
+
+func main() {
+	tcpAddr, err := net.ResolveTCPAddr("tcp", "127.0.0.1:8080")
+	if err != nil {
+		panic(err)
+	}
+	tcpConn, err := net.DialTCP("tcp", nil, tcpAddr)
+	if err != nil {
+		panic(err)
+	}
+	defer tcpConn.Close()
+	// tcpConn.SetKeepAlive(true)
+	buf := new(bytes.Buffer)
+	for i := 1; i < 6; i++ {
+		str := ""
+		for j := 0; j < i; j++ {
+			str += "Hello, world! "
+		}
+		dataLen := len(str)
+		fmt.Println(dataLen)
+		binary.Write(buf, binary.LittleEndian, uint32(dataLen))
+		fmt.Println(str)
+		binary.Write(buf, binary.LittleEndian, []byte(str))
+	}
+	quit := "quit"
+	binary.Write(buf, binary.LittleEndian, uint32(len(quit)))
+	binary.Write(buf, binary.LittleEndian, []byte(quit))
+	tcpConn.Write(buf.Bytes())
+}
---
+++ stdsss/net/tcp/server/server.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"encoding/binary"
+	"fmt"
+	"io"
+	"net"
+)
+
+func main() {
+	// ResolveTCPAddr 主要是验证协议、地址、端口是否正确
+	tcpAddr, err := net.ResolveTCPAddr("tcp", "127.0.0.1:8080")
+	if err != nil {
+		panic(err)
+	}
+	tcpListener, err := net.ListenTCP("tcp", tcpAddr)
+
+	if err != nil {
+		panic(err)
+	}
+	for {
+		conn, err := tcpListener.AcceptTCP()
+		if err != nil {
+			continue
+		}
+		// conn.SetKeepAlive(true)
+		go handleConnect(conn)
+	}
+}
+
+func handleConnect(conn net.Conn) {
+	defer conn.Close()
+	for {
+		// 读固定大小, binary 方便数字与字节之间转换, 根据 数字 类型 大小 去读字节
+		var dataLen uint32
+		binary.Read(conn, binary.LittleEndian, &dataLen)
+
+		fmt.Println("dataLen:", dataLen)
+
+		// 具体非固定大小数据, 需要指定长度读取
+		data := make([]byte, dataLen)
+		io.ReadFull(conn, data)
+
+		str := string(data)
+		fmt.Println("data:", str)
+		if str == "quit" {
+			break
+		}
+	}
+}
+
+/*
+func handleConnect2(conn net.Conn) {
+	defer conn.Close()
+	reader := bufio.NewReader(conn)
+	for {
+		var dataLen uint32
+		binary.Read(reader, binary.LittleEndian, &dataLen)
+
+		fmt.Println("dataLen:", dataLen)
+		data := make([]byte, dataLen)
+		_, err := reader.Read(data)
+		if err != nil {
+			continue
+		}
+		str := string(data)
+		fmt.Println("data: ", str)
+		if str == "quit" {
+			break
+		}
+	}
+}
+*/
