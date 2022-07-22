892b78b72b783c5b277215a5bb8ff6eef634dc2d
GodYao <liyaoo1995@gmail.com>
2022-07-22 16:44:43 +0800

    😀🌴⭐🌎☀️🌈🌽: std encoding/gob

--- stdsss/encoding/gob/gob_test.go
+++ stdsss/encoding/gob/gob_test.go
@@ -29,6 +29,7 @@ func TestGobEncodeAndDecode(t *testing.T) {
 	enc := gob.NewEncoder(buf)
 
 	// 必须先注册 , 因为Message中有 interface{} 类型, 并且赋值的时候可能是 map struct 类型
+	// 大部分的其他类型 已经默认注册进去了
 	gob.Register(map[string]string{})
 
 	request := map[string]string{
@@ -46,11 +47,48 @@ func TestGobEncodeAndDecode(t *testing.T) {
 
 	// Mock Network Transport
 	fmt.Println("Decode:...........................................")
-	
+
 	// "buf" can be networkdata
 	dec := gob.NewDecoder(buf)
 	var message2 MessageReceiver
 	dec.Decode(&message2)
 
 	fmt.Println("Decode Mesaage: ", message2)
+}
+
+func TestGobEncodeAndDecode2(t *testing.T) {
+	// Encode the message
+	fmt.Println("Encode2:...........................................")
+
+	buf := new(bytes.Buffer)
+	enc := gob.NewEncoder(buf)
+	type Request struct {
+		Method string
+		Mode   string
+	}
+	// 先注册
+	gob.Register(Request{})
+
+	request := Request{
+		Method: "gob",
+		Mode:   "encode",
+	}
+	message := MessageSender{
+		ServiceName: "Hello",
+		CallNumber:  1001,
+		Request:     request,
+	}
+
+	enc.Encode(&message)
+	fmt.Println("Encode Mesaage: ", buf)
+
+	// Mock Network Transport
+	fmt.Println("Decode2:...........................................")
+
+	// "buf" can be networkdata
+	dec := gob.NewDecoder(buf)
+	var message2 MessageReceiver
+	dec.Decode(&message2)
+
+	fmt.Println("Decode Mesaage2: ", message2)
 }
