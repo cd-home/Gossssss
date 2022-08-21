ed0e06d6508d02b4c5be5ced3330a28a27c24453
GodYao <liyaoo1995@gmail.com>
2022-08-21 16:14:30 +0800

    😀🌴⭐🌎☀️🌈🌽: Go Functions

--- go.dev/tour/functions/main.go
+++ go.dev/tour/functions/main.go
@@ -3,17 +3,52 @@ package main
 import "fmt"
 
 // Foo name
-// params list
-// return list [support multi return]
-func Foo(param1 string, param2 string) (string, error) {
-	return param1 + param2, nil
+// Params list
+// Return list [support multi return Values]
+func Foo(param1 string, param2 string, x, y int) (string, int, error) {
+	return param1 + param2, x + y, nil
+}
+
+// SameParamsType
+// When two or more consecutive named function parameters share a type,
+// You can omit[省略] the type from all but the last.
+func SameParamsType(x, y int) int {
+	return x + y
+}
+
+// MultiReturnValue
+// Return list [support multi return]
+// A function can return any number of results.
+func MultiReturnValue(x, y int) (int, int) {
+	return y, x
+}
+
+// NamedReturnValue
+// Func return values can be named
+// They are treated as variables defined at the top of the function.
+func NamedReturnValue(x, y int) (sum int) {
+	sum = x + y
+	// return can omit sum, if the func is short
+	// if this is a long func, better to return values
+	return
 }
 
 func main() {
 	// Call Func
-	s, e := Foo("a", "b")
+	str, num, e := Foo("a", "b", 1, 2)
 	if e != nil {
 		panic(e)
 	}
-	fmt.Println(s)
+	fmt.Println(str)
+	fmt.Println(num)
+
+	sums := SameParamsType(1, 2)
+	fmt.Println(sums)
+
+	y, x := MultiReturnValue(2, 5)
+	fmt.Println(x)
+	fmt.Println(y)
+
+	sums = NamedReturnValue(4, 9)
+	fmt.Println(sums)
 }
