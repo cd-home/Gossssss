910d3f327c31628190f6b479a44f10415bb9a125
GodYao <liyaoo1995@gmail.com>
2022-08-27 19:39:38 +0800

    😀🌴⭐🌎☀️🌈🌽: Go Tour Concurrency [Goroutine, Channel]

--- go.dev/tour/concurrency/channel/main.go
+++ go.dev/tour/concurrency/channel/main.go
@@ -2,16 +2,53 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
+// SendAndReceiveChannel
+// Channels are a typed conduit[带类型的管道] through
+// which you can send and receive[发送与接收值] values with the channel operator, <-.
+// By default[无缓冲通道], sends and receives block[阻塞] until the other side is ready.
+// This allows goroutines to synchronize[同步] without explicit locks[互斥锁] or
+// condition variables[条件变量].
+func SendAndReceiveChannel() {
+	sum := func(slices []int, res chan int) {
+		sums := 0
+		for _, v := range slices {
+			sums += v
+		}
+		// mock io
+		time.Sleep(time.Second)
+		// send
+		res <- sums
+	}
+
+	s := []int{7, 2, 8, -9, 4, 0}
+
+	// channels must be created before use
+	// 使用之前必须初始化
+	ch := make(chan int)
+
+	// distributing the work between two goroutines
+	go sum(s[:3], ch)
+	go sum(s[3:], ch)
+
+	// blocked receive
+	x, y := <-ch, <-ch
+	fmt.Println(x, y, x+y)
+}
+
 // BufferedChannel
-// Sends to a buffered channel block only when the buffer is full.
-// Receives block when the buffer is empty.
+// Sends to a buffered channel block only when the buffer is full. 发送阻塞当通道满
+// Receives block when the buffer is empty. 接收者阻塞当通道空
 func BufferedChannel() {
+	// Buffer Channel
 	ch := make(chan int, 10)
-	// Not blocked
+
+	// Not be blocked if channel buffer not full
 	ch <- 1
 	ch <- 2
+	// if sender do not send data, should close channel, or deadlock
 	close(ch)
 
 	// Read Until Close
@@ -20,38 +57,25 @@ func BufferedChannel() {
 	}
 }
 
-func main() {
-	sum := func(slices []int, res chan int) {
-		sums := 0
-		for _, v := range slices {
-			sums += v
-		}
-		res <- sums
-	}
-	s := []int{7, 2, 8, -9, 4, 0}
-	ch := make(chan int)
-	go sum(s[:3], ch)
-	go sum(s[3:], ch)
-	x, y := <-ch, <-ch
-	fmt.Println(x, y, x+y)
-
-	// buffer channel
-	BufferedChannel()
-
+func ReadChannelByForLoop() {
 	// read channel
 	cds := make(chan string, 3)
 	cds <- "Hello World!"
 	cds <- "Go"
 	cds <- "Python"
+	// sender need close if no values to send
 	close(cds)
 	for {
+		// ok is false if there are no more values to receive and the channel is closed.
 		v, ok := <-cds
 		if !ok {
 			break
 		}
 		fmt.Println(ok, v)
 	}
+}
 
+func RangeAndCloseChannel() {
 	// close channel and for-range
 	Fibonacci := func(n int) <-chan int {
 		x, y := 0, 1
@@ -73,4 +97,16 @@ func main() {
 	for v := range Fibonacci(10) {
 		fmt.Println(v)
 	}
+}
+
+func main() {
+	// Default NoBufferedChannel
+	SendAndReceiveChannel()
+
+	// buffer channel
+	BufferedChannel()
+
+	// Read Channel
+	ReadChannelByForLoop()
+
 }
