113dc2d4b27f507580f14bb534f8776024487232
GodYao <liyaoo1995@gmail.com>
2022-09-02 17:55:46 +0800

    😀🌴⭐🌎☀️🌈🌽: Go Stdlib [flag]

--- go.dev/stdlib/flag/main.go
+++ go.dev/stdlib/flag/main.go
@@ -3,61 +3,39 @@ package main
 import (
 	"flag"
 	"fmt"
-	"strings"
+	"time"
 )
 
-type NamesFlag struct {
-	Names []string
-}
+func SimpleFlags() {
+	// support
+	// -from=x  --from=y
+	// -from x  --from y
+	var from = flag.String("from", "ch", "where from")
 
-func (s *NamesFlag) GetNames() []string {
-	return s.Names
-}
+	// support true [True] false [False]
+	var stop = flag.Bool("stop", false, "stop")
 
-func (s *NamesFlag) String() string {
-	return fmt.Sprint(s.Names)
-}
-
-func (s *NamesFlag) Set(v string) error {
-	if len(s.Names) > 0 {
-		return fmt.Errorf("no")
-	}
-	names := strings.Split(v, ",")
-	s.Names = append(s.Names, names...)
-	return nil
-}
-
-func MoreFlags() {
-	var manyNames NamesFlag
-	minusk := flag.Int("k", 0, "An int")
-	minusO := flag.String("o", "nike", "name")
-	flag.Var(&manyNames, "names", "names list")
+	// support 1h 1h2m
+	var dur = flag.Duration("time", time.Hour, "")
 
 	flag.Parse()
-	fmt.Println("-k", *minusk)
-	fmt.Println("-o", *minusO)
-
-	for idx, name := range manyNames.GetNames() {
-		fmt.Println(idx, name)
-	}
-
-	for idx, args := range flag.Args() {
-		fmt.Println(idx, args)
-	}
+	fmt.Println(*from)
+	fmt.Println(*stop)
+	fmt.Println(*dur)
 }
 
-func SimpleFlags() {
+func VarFlags() {
 	var ip string
 	var port string
 	flag.StringVar(&ip, "ip", "127.0.0.1", "IP address")
 	flag.StringVar(&port, "port", ":8080", "Port number")
 	flag.Parse()
-
 	// ./flag -ip=127.0.0.1 -port=8989
 	fmt.Println(ip, port)
 }
 
 func main() {
-	// go run stdsss/flag/main.go -o 111 -k 222 -names mike,jack
-	MoreFlags()
+	//SimpleFlags()
+	VarFlags()
+	//MoreFlags()
 }
