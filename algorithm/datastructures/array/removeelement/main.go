9bc9d2924a40def49a8d6626c95f146ccd667594
GodYao <liyaoo1995@gmail.com>
2022-11-04 15:17:04 +0800

    😀🌴⭐🌎☀️🌈🌽: Update Algs [Array-removeElement]

--- algorithm/datastructures/array/removeelement/main.go
+++ algorithm/datastructures/array/removeelement/main.go
@@ -7,6 +7,12 @@ func main() {
 	fmt.Println(removeElement(arr, 3))
 }
 
+// 给你一个数组 num 和一个值 val，你需要 原地 移除所有数值等于val的元素，并返回移除后数组的新长度。
+// 不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并 原地 修改输入数组。
+// 元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+
+// removeElement
+// 原地移除、考虑异常情况
 func removeElement(nums []int, val int) int {
 	// 数组为空的情况
 	if len(nums) == 0 {
