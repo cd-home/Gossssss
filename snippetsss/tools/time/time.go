fb0f9bc522f93bfb75e4db433dccd095d84b0904
GodYao <liyaoo1995@gmail.com>
2022-11-19 22:42:00 +0800

    😀🌴⭐🌎☀️🌈🌽: Update Snippetsss Tools [time]

--- snippetsss/tools/time/time.go
+++ snippetsss/tools/time/time.go
@@ -1,1 +1,12 @@
 package time
+
+import "time"
+
+func PastNDay(n int) []string {
+	now := time.Now()
+	days := make([]string, n)
+	for i := n; i > 0; i-- {
+		days[n-i] = now.AddDate(0, 0, -i).Format("2006-01-02")
+	}
+	return days
+}
