57bf05459ae47b2961609df423234351d60ca7a9
GodYao <liyaoo1995@gmail.com>
2022-11-19 22:41:28 +0800

    😀🌴⭐🌎☀️🌈🌽: Update Go Programming [Methods]

--- books/goprogramming/methods/main.go
+++ books/goprogramming/methods/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"math"
+)
+
 func main() {
 
+}
+
+type Point struct {
+	X, Y float64
+}
+
+// Distance
+// Point Receiver 接收器 [简短、一致] 通常采用类型的某几个字母
+func (p Point) Distance(q Point) float64 {
+	return math.Hypot(q.X-p.X, q.Y-p.Y)
 }
