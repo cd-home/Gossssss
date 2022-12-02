44d0d4488c9a1497e11eba64d478d6e461282a92
GodYao <liyaoo1995@gmail.com>
2022-12-02 18:07:55 +0800

    😀🌴⭐🌎☀️🌈🌽: Update Go Programming [Packages, Tools]

--- books/goprogramming/packages/main.go
+++ books/goprogramming/packages/main.go
@@ -2,23 +2,24 @@ package main
 
 func main() {
 	/*
-			1. 包组织代码、设计系统、维护、重用
-			2. 包名简短、API大小写开放特性
-			3. 禁止包之间循环依赖
+		1. 包组织代码、设计系统、维护、重用
+		2. 包名简短、API大小写开放特性
+		3. 禁止包之间循环依赖
 
-			4. import _ [init]  import reName
-			5. 包名简短、有意义、一般采用单数
+		4. import _ [init]  import reName
+		5. 包名简短、有意义、一般采用单数
+			internal 包特殊 同级包不能导入internal
 
-			6. cmd tools
-				go run
-		 		go build // 跨平台编译
-					GOOS=linux GOARCH=amd64 CGO_ENABLED=0
-				go test
-				go fmt
-				go mod
-				go get
-				go install // 编译到 GOPATH/bin下
-				go vet
-				go doc     // go doc time
+		6. cmd tools
+			go run
+			go build // 跨平台编译
+				GOOS=linux GOARCH=amd64 CGO_ENABLED=0
+			go test
+			go fmt
+			go mod
+			go get
+			go install // 编译到 GOPATH/bin下
+			go vet
+			go doc     // go doc time
 	*/
 }
