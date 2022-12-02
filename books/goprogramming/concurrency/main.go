8f100c6b61f6eca5a5b1118e6af7287b79a651c9
GodYao <liyaoo1995@gmail.com>
2022-12-02 17:07:39 +0800

    😀🌴⭐🌎☀️🌈🌽: Update Go Programming [Concurrency]

--- books/goprogramming/concurrency/main.go
+++ books/goprogramming/concurrency/main.go
@@ -11,13 +11,15 @@ func main() {
 			go run -race
 			go test -race
 		4. 线程
-			2MB Thread 栈
-			2KM goroutine 动态伸缩 最大1GB
-
-			线程 调度 硬件计时器
-				挂起 寄存器 => 内存; 运行 内存=> 寄存器;
-			协程 Runtime
-				N:M, 调度goroutine 不需要进入内核的上下文
+			动态栈
+				2MB Thread 栈
+				2KB goroutine 动态伸缩 最大1GB
+			调度
+				线程 调度 硬件计时器
+					挂起 寄存器 => 内存; 运行 内存=> 寄存器;
+				协程 Runtime
+					N:M, 调度goroutine 不需要进入内核的上下文
+					阻塞操作、强制让出等
 			GOMAXPROCS
 				决定多个核心执行Go代码 即是 N
 			Goroutine
