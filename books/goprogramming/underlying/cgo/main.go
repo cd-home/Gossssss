2147c3e631caa83bd614779dd32355afcb731432
GodYao <liyaoo1995@gmail.com>
2022-12-03 21:44:21 +0800

    😀🌴⭐🌎☀️🌈🌽: Update Go Programming [Underlying, Cgo]

--- books/goprogramming/underlying/cgo/main.go
+++ books/goprogramming/underlying/cgo/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+//void SayHello(const char* s);
+import "C"
 
+func main() {
+	C.SayHello(C.CString("Hello, World"))
 }
+
+/*
+#include <stdio.h>
+
+static void SayHello(const char* s) {
+    puts(s);
+}
+*/
