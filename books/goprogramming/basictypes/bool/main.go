c8a02c56c2840f6ada5ba3edd8d97eb102177e81
GodYao <liyaoo1995@gmail.com>
2022-12-07 21:22:52 +0800

    😀🌴⭐🌎☀️🌈🌽: Update Go Programming [BasicTypes, Bool]

--- books/goprogramming/basictypes/bool/main.go
+++ books/goprogramming/basictypes/bool/main.go
@@ -1,9 +1,18 @@
 package main
 
+import "fmt"
+
 func main() {
 	/*
 		布尔类型
 			true、false
 			支持 && || !三种逻辑运算
 	*/
+	var b1 = true
+	var b2 = false
+	fmt.Println(b1)
+	fmt.Println(b2)
+	fmt.Println(b1 && b2)
+	fmt.Println(b1 || b2)
+	fmt.Println(!b1)
 }
