73c831398cfa5ec3a644ce9985d8f50b8b0e82fe
GodYao <liyaoo1995@gmail.com>
2022-12-10 23:05:15 +0800

    😀🌴⭐🌎☀️🌈🌽: Update Algorithm [BasicAlgs, Sorting]

--- algorithm/basicsalgs/sorting/bubble/main.go
+++ algorithm/basicsalgs/sorting/bubble/main.go
@@ -2,16 +2,19 @@ package main
 
 import "fmt"
 
+// BubbleSort
+// 冒泡排序
 func BubbleSort(arr []int) {
 	l := len(arr)
+	// 检查入参
 	if l <= 1 {
 		return
 	}
 	// 轮数
 	for i := 0; i < l; i++ {
-		// 每一轮做什么, 一个数和剩下的比较
 		// 每一轮进来定义一个是否有数据交换的标志
 		flag := false
+		// 每一轮做什么, 一个数和剩下的比较
 		for j := 0; j < l-1; j++ {
 			if arr[j] > arr[j+1] {
 				arr[j], arr[j+1] = arr[j+1], arr[j]
