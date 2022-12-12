cddda7e81465f68d07526fa312ceb6efa01cd82c
GodYao <liyaoo1995@gmail.com>
2022-12-12 23:38:55 +0800

    😀🌴⭐🌎☀️🌈🌽: Update Go.dev [Server, DefineYourServer]

--- go.dev/tour/http/examples/server.go
+++ go.dev/tour/http/examples/server.go
@@ -16,29 +16,27 @@ func YourselfServer(rw http.ResponseWriter, r *http.Request) {
 }
 
 func DefineYourServer() {
-
 	mux := http.DefaultServeMux
-	// mux := http.NewServeMux()
-
 	mux.HandleFunc("/", YourselfServer)
-
+	// 自定义服务
 	server := &http.Server{
 		Addr:         ":8080",
 		Handler:      mux,
 		ReadTimeout:  3 * time.Second,
 		WriteTimeout: 3 * time.Second,
 	}
+	// 启动服务
 	go func() {
 		if err := server.ListenAndServe(); err != nil {
 			log.Fatal(err)
 		}
 	}()
-
+	// 信号退出
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 	<-quit
-	ctx, cancle := context.WithTimeout(context.Background(), time.Second*5)
-	defer cancle()
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
 
 	if err := server.Shutdown(ctx); err != nil {
 		log.Printf("Shutting down server: %s", err.Error())
