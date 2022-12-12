fc4bf89553fbddd68a24ef225de7f19caa3ad8e1
GodYao <liyaoo1995@gmail.com>
2022-12-12 23:44:09 +0800

    😀🌴⭐🌎☀️🌈🌽: Update Go.dev [Server, Request]

---
+++ go.dev/tour/http/examples/param.go
@@ -0,0 +1,36 @@
+package http
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func GetRequest(writer http.ResponseWriter, request *http.Request) {
+	// Form-Data Body Fields
+	//request.ParseForm()
+	//request.PostForm.Get()
+
+	// Not include URL (POST)
+	//request.PostFormValue()
+
+	// URL AND Form-Data Body
+	//request.ParseForm()
+	//request.Form.Get()
+
+	// Use this OK !
+	//request.FormValue()
+	val := request.FormValue("name")
+	fmt.Println(val)
+
+	// One
+	//request.ParseMultipartForm()
+	//request.MultipartForm.File[""]
+
+	// Two
+	//f, header, _ := request.FormFile("upload")
+}
+
+func GetRequestServer() {
+	http.HandleFunc("/params", GetRequest)
+	http.ListenAndServe(":8080", nil)
+}
