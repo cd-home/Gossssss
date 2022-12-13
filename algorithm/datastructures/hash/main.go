b0177e3c18bc62ff16144c56289f8c38354548e4
GodYao <liyaoo1995@gmail.com>
2022-12-13 21:42:21 +0800

    😀🌴⭐🌎☀️🌈🌽: Update Algorithm [DataStructures, Hash/Map]

--- algorithm/datastructures/hash/main.go
+++ algorithm/datastructures/hash/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	// 映射 Map
+	// 键值存储 k-v
+	m := make(map[int]string, 5)
+	m[1] = "one"
+	m[2] = "two"
+	m[3] = "three"
+	for k, v := range m {
+		fmt.Println(k, v)
+	}
 }
