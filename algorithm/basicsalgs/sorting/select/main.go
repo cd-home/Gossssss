ce98eee848cb8852d270dd2b9b1fc990b16da1c6
GodYao <liyaoo1995@gmail.com>
2023-02-11 00:27:28 +0800

    😀🌴⭐🌎☀️🌈🌽: [basicalgs] selectionsort

--- algorithm/basicsalgs/sorting/select/main.go
+++ algorithm/basicsalgs/sorting/select/main.go
@@ -4,15 +4,17 @@ import "fmt"
 
 func SelectionSort(arr []int) {
 	n := len(arr)
-	// n 个元素， n-1次循环即可
+	// n 个 元素 只需要 确定 n-1 个元素大小
 	for i := 0; i < n-1; i++ {
 		min := i
-		// 找最小
+		// i 与 i + 1 后面大小比较
 		for j := i + 1; j < n; j++ {
 			if arr[j] < arr[min] {
+				// 一直找到最小的
 				min = j
 			}
 		}
+		// 交换 exchange
 		arr[i], arr[min] = arr[min], arr[i]
 	}
 }
