c83b364c0b572ba7256b0fba70ff46add868a691
GodYao <liyaoo1995@gmail.com>
2023-02-11 19:54:45 +0800

    😀🌴⭐🌎☀️🌈🌽: [basicalgs] update bubblesort

--- algorithm/basicsalgs/sorting/bubble/main.go
+++ algorithm/basicsalgs/sorting/bubble/main.go
@@ -5,13 +5,13 @@ import "fmt"
 // BubbleSort
 // 冒泡排序
 func BubbleSort(arr []int) {
-	l := len(arr)
+	n := len(arr)
 	// 轮数
-	for i := 0; i < l; i++ {
+	for i := 0; i < n; i++ {
 		// 每一轮进来定义一个是否有数据交换的标志
 		flag := false
 		// 每一轮比较交换, 这一轮能得到一个最值
-		for j := 0; j < l-1; j++ {
+		for j := 0; j < n-1; j++ {
 			if arr[j] > arr[j+1] {
 				arr[j], arr[j+1] = arr[j+1], arr[j]
 				flag = true
