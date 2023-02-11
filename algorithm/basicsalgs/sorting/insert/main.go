87f834629bdc6491d77c32bd2fcda80df6b2d5eb
GodYao <liyaoo1995@gmail.com>
2023-02-11 19:47:07 +0800

    😀🌴⭐🌎☀️🌈🌽: [basicalgs] update insertionsort

--- algorithm/basicsalgs/sorting/insert/main.go
+++ algorithm/basicsalgs/sorting/insert/main.go
@@ -4,20 +4,21 @@ import "fmt"
 
 func InsertionSort(arr []int) {
 	n := len(arr)
+	// 从1开始, 假定0位置有序
 	for i := 1; i < n; i++ {
 		cur := arr[i]
-		j := i - 1
+		j := i
 		// 0 ~ j 的位置已经排好序
-		for ; j >= 0 && arr[j] > cur; j-- {
+		for ; j >= 1 && arr[j-1] > cur; j-- {
 			// 后移一位
-			arr[j+1] = arr[j]
+			arr[j] = arr[j-1]
 		}
-		arr[j+1] = cur
+		arr[j] = cur
 	}
 }
 
 func main() {
-	arr := []int{5, 2, 3, 1, 4}
+	arr := []int{5, 2, 3, 1, 4, 20, 10, 19, 23, 11, 25, 98, 23, 3, 4, 123}
 	InsertionSort(arr)
 	fmt.Println(arr)
 }
---
+++ algorithm/basicsalgs/sorting/shell/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func ShellSort(arr []int) {
+	n := len(arr)
+	for increment := n / 2; increment > 0; increment /= 2 {
+		// 插入排序
+		for i := increment; i < n; i++ {
+			tmp := arr[i]
+			j := i
+			// 比较移位
+			for j = i; j >= increment && arr[j-increment] > tmp; j -= increment {
+				arr[j] = arr[j-increment]
+			}
+			// 赋值
+			arr[j] = tmp
+		}
+	}
+}
+
+func main() {
+	arr := []int{5, 2, 3, 1, 4, 20, 10, 19, 23, 11, 25, 98, 23, 3, 4, 123}
+	ShellSort(arr)
+	fmt.Println(arr)
+}
