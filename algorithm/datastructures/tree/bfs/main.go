e49be647da8795d22bb6155a1e551e0b3482418a
GodYao <liyaoo1995@gmail.com>
2023-02-15 21:06:38 +0800

    😀🌴⭐🌎☀️🌈🌽: [datastrutures] tree bfs levelorder

---
+++ algorithm/datastructures/tree/bfs/main.go
@@ -0,0 +1,34 @@
+package main
+
+type TreeNode struct {
+	Data  int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func levelOrder(root *TreeNode) [][]int {
+	var res [][]int
+	// 队列
+	var queue []*TreeNode
+	queue = append(queue, root)
+	for len(queue) > 0 {
+		var temp []int
+		for i := 0; i < len(queue); i++ {
+			node := queue[0]
+			queue = queue[1:]
+			temp = append(temp, node.Data)
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+		res = append(res, temp)
+	}
+	return res
+}
+
+func main() {
+
+}
