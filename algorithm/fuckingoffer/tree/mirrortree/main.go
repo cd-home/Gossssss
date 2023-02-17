1f13c21a8f8543b9c276f17d305e0e13a9ec1ead
GodYao <liyaoo1995@gmail.com>
2023-02-17 22:50:01 +0800

    😀🌴⭐🌎☀️🌈🌽: [fucking-offer] mirror tree

--- algorithm/fuckingoffer/tree/mirrortree/main.go
+++ algorithm/fuckingoffer/tree/mirrortree/main.go
@@ -1,5 +1,25 @@
 package main
 
+// 剑指 Offer 27. 二叉树的镜像
+
+/*
+	请完成一个函数，输入一个二叉树，该函数输出它的镜像。
+	例如输入：
+
+	  4
+	 /  \
+	 2   7
+	/ \  / \
+	1  3 6  9
+	镜像输出：
+
+	  4
+	 /  \
+	 7   2
+	/ \  / \
+	9  6 3 1
+*/
+
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
@@ -10,7 +30,7 @@ func mirrorTree(root *TreeNode) *TreeNode {
 	if root == nil {
 		return nil
 	}
-	// 从下往上, 先找到叶子节点
+	// 先找到叶子节点, 从下往上交换左右孩子
 	left := mirrorTree(root.Left)
 	right := mirrorTree(root.Right)
 	root.Left = right
