ca7bb7ddbc732dac667e28720641bd8a02f2f705
GodYao <liyaoo1995@gmail.com>
2023-02-18 00:24:46 +0800

    😀🌴⭐🌎☀️🌈🌽: [datastrutures] update binary search tree

--- algorithm/datastructures/tree/bst/main.go
+++ algorithm/datastructures/tree/bst/main.go
@@ -2,134 +2,133 @@ package main
 
 import "fmt"
 
-// Tree
-// 二叉查找树(二叉搜索树)  left <= root < right
-type Tree struct {
-	root *node
+// TreeNode 二叉搜索树、二叉查找树
+type TreeNode struct {
+	Data  int
+	Left  *TreeNode
+	Right *TreeNode
 }
 
-// tree node
-type node struct {
-	val   int
-	left  *node
-	right *node
-}
-
-func (t *Tree) Add(val int) {
-	if t.root == nil {
-		t.root = &node{val: val}
-		return
-	}
-	// 每次添加都从root开始判断,二分
-	t.root.add(val)
-}
-
-func (n *node) add(val int) {
-	// 左子树
-	if val <= n.val {
-		// 如果左子树存在, 递归
-		if n.left != nil {
-			n.left.add(val)
+// FindX 循环
+func FindX(node *TreeNode, data int) *TreeNode {
+	for node != nil {
+		if data < node.Data {
+			node = node.Left
+		} else if data > node.Data {
+			node = node.Right
 		} else {
-			n.left = &node{val: val}
+			return node
 		}
-		return
-	}
-	// 右子树
-	if val > n.val {
-		// 如果右子树存在, 递归
-		if n.right != nil {
-			n.right.add(val)
-		} else {
-			n.right = &node{val: val}
-		}
-		return
-	}
-}
-
-func (t *Tree) Find(val int) *node {
-	if t.root == nil {
-		return nil
-	}
-	return t.root.find(val)
-}
-
-func (n *node) find(val int) *node {
-	if n == nil {
-		return nil
-	}
-	if val == n.val {
-		return n
-	}
-	// 二分查找
-	if val < n.val {
-		return n.left.find(val)
-	}
-	if val > n.val {
-		return n.right.find(val)
 	}
 	return nil
 }
 
-func (t *Tree) FindMin() *node {
-	if t.root == nil {
+// FindY 递归
+func FindY(node *TreeNode, data int) *TreeNode {
+	if node == nil {
 		return nil
 	}
-	return t.root.findMin()
+	if node.Data == data {
+		return node
+	}
+	if data < node.Data {
+		return FindY(node.Left, data)
+	} else {
+		return FindY(node.Right, data)
+	}
 }
 
-func (n *node) findMin() *node {
-	if n == nil {
-		return nil
-	}
-	min := n
-	for min.left != nil {
-		min = min.left
-	}
-	return min
-}
-
-func (t *Tree) FindMax() *node {
-	if t.root == nil {
-		return nil
-	}
-	return t.root.findMax()
-}
-
-func (n *node) findMax() *node {
-	if n == nil {
-		return nil
-	}
-	max := n
-	for max.right != nil {
-		max = max.right
-	}
-	return max
-}
-
-func (t *Tree) inorderTraversal() []int {
-	var res []int
-	var inOrder func(*node, *[]int)
-	inOrder = func(t *node, res *[]int) {
-		if t != nil {
-			inOrder(t.left, res)
-			*res = append(*res, t.val)
-			inOrder(t.right, res)
+func FindMaxX(node *TreeNode) *TreeNode {
+	if node != nil {
+		for node.Right != nil {
+			node = node.Right
 		}
 	}
-	inOrder(t.root, &res)
+	return node
+}
+
+func FindMaxY(node *TreeNode) *TreeNode {
+	if node == nil {
+		return nil
+	}
+	if node.Right != nil {
+		return FindMaxY(node.Right)
+	} else {
+		return node
+	}
+}
+
+func FindMinX(node *TreeNode) *TreeNode {
+	if node != nil {
+		for node.Left != nil {
+			node = node.Left
+		}
+	}
+	return node
+}
+
+func FindMinY(node *TreeNode) *TreeNode {
+	if node == nil {
+		return nil
+	}
+	if node.Left != nil {
+		return FindMinY(node.Left)
+	} else {
+		return node
+	}
+}
+
+func Insert(node *TreeNode, data int) *TreeNode {
+	if node == nil {
+		node = &TreeNode{Data: data}
+	} else {
+		if data < node.Data {
+			node.Left = Insert(node.Left, data)
+		} else {
+			node.Right = Insert(node.Right, data)
+		}
+	}
+	return node
+}
+
+func Delete(node *TreeNode, data int) {
+
+}
+
+func InOrderTraversal(root *TreeNode) []int {
+	var res []int
+	var inOrder func(*TreeNode, *[]int)
+	inOrder = func(node *TreeNode, res *[]int) {
+		if node != nil {
+			inOrder(node.Left, res)
+			*res = append(*res, node.Data)
+			inOrder(node.Right, res)
+		}
+	}
+	inOrder(root, &res)
 	return res
 }
 
 func main() {
-	arr := []int{9, 2, 2, 3, 10, 28, 1, 4}
-	t := new(Tree)
-	for _, v := range arr {
-		t.Add(v)
-	}
-	fmt.Println(t.inorderTraversal())
+	root := &TreeNode{Data: 8}
+	Insert(root, 4)
+	Insert(root, 5)
+	Insert(root, 3)
+	Insert(root, 9)
+	Insert(root, 1)
+	Insert(root, 2)
+	Insert(root, 10)
+	Insert(root, 17)
 
-	fmt.Println(t.Find(9))
-	fmt.Println(t.FindMin())
-	fmt.Println(t.FindMax())
+	// 中序遍历
+	fmt.Println(InOrderTraversal(root))
+
+	fmt.Println(FindMaxX(root))
+	fmt.Println(FindMaxY(root))
+
+	fmt.Println(FindMinX(root))
+	fmt.Println(FindMinY(root))
+
+	fmt.Println(FindX(root, 10))
+	fmt.Println(FindY(root, 10))
 }
