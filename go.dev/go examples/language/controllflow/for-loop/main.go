3593f8fb9021f4ab5aaed629e2d8b927bec1c7a4
GodYao <liyaoo1995@gmail.com>
2023-02-24 06:33:16 +0800

    😀🌴⭐🌎☀️🌈🌽: [Go examples] language controllflow for-loop

--- go.dev/go examples/language/controllflow/for-loop/main.go
+++ go.dev/go examples/language/controllflow/for-loop/main.go
@@ -2,26 +2,39 @@ package main
 
 import "fmt"
 
+/*
+	go 唯一的循环控制结构
+*/
+
 func main() {
+	// 类似 while
 	i := 1
 	for i <= 3 {
 		fmt.Println(i)
 		i++
 	}
 
+	// 经典的 init condition after
 	for j := 0; j <= 9; j++ {
 		fmt.Println(j)
 	}
 
+	// for-ever 死循环
 	for {
 		fmt.Println("loop")
 		break
 	}
 
+	// break continue 用于跳出循环
 	for n := 0; n <= 5; n++ {
-		if n % 2 == 0 {
+		if n%2 == 0 {
 			continue
 		}
 		fmt.Println(n)
+	}
+
+	// 多个变量
+	for i, j := 0, 10; i < j; i, j = i+1, j-1 {
+
 	}
 }
