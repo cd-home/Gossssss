7450d89cb4b4c2763b056719ba4def85cc016568
GodYao <liyaoo1995@gmail.com>
2023-02-25 00:00:26 +0800

    😀🌴⭐🌎☀️🌈🌽: [Go examples] codesnippets goroutine workers

--- go.dev/goexamples/codesnippets/worker/main.go
+++ go.dev/goexamples/codesnippets/worker/main.go
@@ -5,7 +5,8 @@ import (
 	"time"
 )
 
-func worker(id int, jobs <-chan int, r chan<- int)  {
+func worker(id int, jobs <-chan int, r chan<- int) {
+	// 注意这里就使用了只读的 通道 jobs, r 只写
 	// 并发的worker都去jobs通道中获取任务
 	for job := range jobs {
 		fmt.Println("worker(id):", id)
@@ -16,7 +17,7 @@ func worker(id int, jobs <-chan int, r chan<- int)  {
 }
 
 func main() {
-	const JobsNums = 5
+	const JobsNums = 1000
 	jobs := make(chan int, JobsNums)
 	results := make(chan int, JobsNums)
 
@@ -26,9 +27,16 @@ func main() {
 
 	for i := 0; i < JobsNums; i++ {
 		jobs <- i
+		// 写完关闭;
+		// 一个适用的原则是不要从接收端关闭channel，也不要在多个并发发送端中关闭channel
+		// 并且禁止关闭只读通道
+		// 发送端关闭
 	}
+	close(jobs)
 
 	for i := 0; i < JobsNums; i++ {
 		fmt.Println(<-results)
 	}
+	// 读取完成
+	close(results)
 }
