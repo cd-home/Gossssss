766b139efd4ed4bf325c84b82e6eef83d8be01a4
GodYao <liyaoo1995@gmail.com>
2023-02-25 00:25:30 +0800

    😀🌴⭐🌎☀️🌈🌽: [Go Web Programming] assets service

--- go.dev/webprogramming/assets/main.go
+++ go.dev/webprogramming/assets/main.go
@@ -1,12 +1,12 @@
-package http
+package main
 
 import (
 	"net/http"
 )
 
-func FileServer() {
+func main() {
+	// 静态文件服务
 	fs := http.FileServer(http.Dir("static/"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
-
-	http.ListenAndServe(":8080", nil)
+	_ = http.ListenAndServe(":8080", nil)
 }
