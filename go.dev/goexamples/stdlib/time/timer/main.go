78f6c404bfe63b5da44bd0bc38924fcbd1cad7c0
GodYao <liyaoo1995@gmail.com>
2023-02-24 23:05:38 +0800

    😀🌴⭐🌎☀️🌈🌽: [Go examples] stdlib time/timer

--- go.dev/goexamples/stdlib/time/timer/main.go
+++ go.dev/goexamples/stdlib/time/timer/main.go
@@ -7,10 +7,13 @@ import (
 
 func main() {
 	// 定时器
+	// timer 在将来某个时间点 回复你当前的时间
 	timer1 := time.NewTimer(2 * time.Second)
 
-	<-timer1.C
-	fmt.Println("Timer 1 fired")
+	select {
+	case t := <-timer1.C:
+		fmt.Println("Timer 1 fired ", t)
+	}
 
 	timer2 := time.NewTimer(time.Second)
 
@@ -21,8 +24,6 @@ func main() {
 	// 可以在某些情况下停止掉timer
 	stop2 := timer2.Stop()
 	if stop2 {
-		fmt.Println("Timer 2 Stoped")
+		fmt.Println("Timer 2 Stopped")
 	}
-
-	time.Sleep(time.Second * 2)
 }
