0c5bf3d5a2c0b69eb883d0f0a76050f1d208a2cf
GodYao <liyaoo1995@gmail.com>
2023-02-24 22:29:43 +0800

    😀🌴⭐🌎☀️🌈🌽: [Go examples] language directions channel

--- go.dev/goexamples/language/goroutines/channels/directions/main.go
+++ go.dev/goexamples/language/goroutines/channels/directions/main.go
@@ -2,11 +2,11 @@ package main
 
 import "fmt"
 
-func Ping(pings chan<- string, msg string)  {
+func Ping(pings chan<- string, msg string) {
 	pings <- msg
 }
 
-func Pong(pings <-chan string, pongs chan<- string)  {
+func Pong(pings <-chan string, pongs chan<- string) {
 	msg := <-pings
 	pongs <- msg
 }
@@ -16,6 +16,7 @@ func main() {
 	pings := make(chan string, 1)
 	pongs := make(chan string, 1)
 
+	// 有方向的通道通常作为函数参数, 这样可以在函数内部做到读、写的安全性
 	Ping(pings, "passed message")
 	Pong(pings, pongs)
 
