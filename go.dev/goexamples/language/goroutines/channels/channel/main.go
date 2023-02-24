f7efb07e8df0398bfcc0cbf7d2c6f1293a5d052f
GodYao <liyaoo1995@gmail.com>
2023-02-24 18:42:49 +0800

    😀🌴⭐🌎☀️🌈🌽: [Go examples] language buffer and unbuffer channel

--- go.dev/goexamples/language/goroutines/channels/channel/main.go
+++ go.dev/goexamples/language/goroutines/channels/channel/main.go
@@ -3,18 +3,24 @@ package main
 import "fmt"
 
 func main() {
+	// channel 是连接并发goroutines 的管道, 用于goroutines 之间的通信、数据传输
+
+	// 创建通道
 	// 创建无缓存通道, 必须有两端准备好
-	ch := make(chan int)
+	messages := make(chan string)
 
 	go func() {
-		// 往通道中写
-		ch <- 100
+		// 写入操作
+		messages <- "Hello"
 	}()
 
-	// 阻塞等待通道数据，读数据
-	fmt.Println(<-ch)
+	// 读取操作
+	// 由于是无缓冲的通道, 会阻塞等待知道发送端、接收端准备好
+	// 是一种靠通道的同步机制
+	msg := <-messages
+	fmt.Println(msg)
 
-
+	// 缓冲通道
 	bufferCh := make(chan string, 2)
 
 	// 无需等接收方，可以先缓存
