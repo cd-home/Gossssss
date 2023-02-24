7ba40d7a9c2a12a9de4fd4ccc50f96192afdb6a1
GodYao <liyaoo1995@gmail.com>
2023-02-25 00:43:59 +0800

    😀🌴⭐🌎☀️🌈🌽: [Go Web Programming] define yourself server

--- go.dev/webprogramming/server/main.go
+++ go.dev/webprogramming/server/main.go
@@ -1,16 +1,44 @@
 package main
 
 import (
-	api "Gossssss/go.dev/webprogramming/files"
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
+func YourselfServer(rw http.ResponseWriter, r *http.Request) {
+	_, _ = fmt.Fprintf(rw, "YourselfServer!, Req: %s", r.URL)
+}
+
 func main() {
-	//api.SimpleServer()
-	// api.FileServer()
-	// api.RouterServer()
-	// api.JSONServer()
-	// api.CookieServer()
-	// api.TemplateServer()
-	// api.DefineYourServer()
-	api.FileUpAndDownServer()
+	mux := http.DefaultServeMux
+	mux.HandleFunc("/", YourselfServer)
+	// 自定义服务
+	server := &http.Server{
+		Addr:         ":8080",
+		Handler:      mux,
+		ReadTimeout:  3 * time.Second,
+		WriteTimeout: 3 * time.Second,
+	}
+	// 启动服务
+	go func() {
+		if err := server.ListenAndServe(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+	// 信号退出
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Printf("Shutting down server: %s", err.Error())
+	}
 }
