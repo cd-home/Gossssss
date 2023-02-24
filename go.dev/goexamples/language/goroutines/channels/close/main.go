1c3968c311bc4a128076561b2d1b2cd362f460c3
GodYao <liyaoo1995@gmail.com>
2023-02-24 22:51:01 +0800

    😀🌴⭐🌎☀️🌈🌽: [Go examples] language close channel

--- go.dev/goexamples/language/goroutines/channels/close/main.go
+++ go.dev/goexamples/language/goroutines/channels/close/main.go
@@ -10,6 +10,7 @@ func main() {
 		for {
 			// more是布尔值，表示通道是否有数据
 			job, more := <-jobs
+			// 关闭后的通道是可以读取的, 当通道关闭并且没有数据读取时: more==false
 			if more {
 				fmt.Println("recv: ", job)
 			} else {
@@ -24,7 +25,8 @@ func main() {
 		jobs <- i
 		fmt.Println("send job: ", i)
 	}
-	// 发送完成 关闭通道
+	// 发送完成 根据需要关闭通道, 假设后续不再往里面写入数据
+	// 也是一种通知, 消息沟通
 	close(jobs)
 	<-done
 }
