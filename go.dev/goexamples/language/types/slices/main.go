c173760e7b45215f74fa34c675105b8c3d998663
GodYao <liyaoo1995@gmail.com>
2023-02-24 16:01:35 +0800

    😀🌴⭐🌎☀️🌈🌽: [Go examples] language types slices

--- go.dev/goexamples/language/types/slices/main.go
+++ go.dev/goexamples/language/types/slices/main.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 func main() {
+	// 切片不需要指定长度, 可以动态增长
+	// nil slice
 	var s []int
 	fmt.Println(s)
 
@@ -14,10 +16,11 @@ func main() {
 	s1[2] = 3
 	fmt.Println(s1)
 
+	// 指定 len=cap=3
 	s2 := make([]string, 3)
-	s2[0]= "a"
-	s2[1]= "b"
-	s2[2]= "c"
+	s2[0] = "a"
+	s2[1] = "b"
+	s2[2] = "c"
 	fmt.Println(s2)
 
 	// 超过容量
@@ -26,9 +29,26 @@ func main() {
 	s2 = append(s2, "f")
 	fmt.Println(s2)
 
+	// len 返回切片的长度
 	s3 := make([]string, len(s2))
+
+	// 支持复制函数
 	copy(s3, s2)
 	fmt.Println(s3)
 
+	// 支持子切片 [i:j]  0 <= i <= j < cap(), 注意左闭右开
+	// i 不写默认0 j 不写默认是len
 	fmt.Println(s3[1:2])
+
+	// 切片字面量
+	t := []string{"g", "h", "i"}
+	fmt.Println(t)
+
+	// 多维 nxm
+	twoD := make([][]int, 3)
+	for i := 0; i < 3; i++ {
+		row := make([]int, 3)
+		twoD[i] = row
+	}
+	fmt.Println(twoD)
 }
