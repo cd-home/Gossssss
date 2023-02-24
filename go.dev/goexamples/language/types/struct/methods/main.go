bfbd6e9bb01a0186c3248e1b761ab3d4f4ec7d94
GodYao <liyaoo1995@gmail.com>
2023-02-24 17:25:58 +0800

    😀🌴⭐🌎☀️🌈🌽: [Go examples] language struct methods

--- go.dev/goexamples/language/types/struct/methods/main.go
+++ go.dev/goexamples/language/types/struct/methods/main.go
@@ -7,8 +7,8 @@ func main() {
 		Width:  10,
 		Height: 10,
 	}
-	// 无论是值receiver 还是指针receiver， 值类型或者指针类型都可以调用
-	// 会自动处理值和指针之间的转换
+	// 无论是值receiver 还是指针receiver， 值类型或者指针类型实例都可以调用
+	// Go 会自动处理值和指针之间的转换 以此来进行方法的调用
 	fmt.Println(r.Area())
 
 	fmt.Println(r.Perimeter())
@@ -23,13 +23,15 @@ type Rect struct {
 	Width, Height int
 }
 
-// *Rect类型的receiver
-// Method
-func (r *Rect) Area() int {
-	return r.Width * r.Height
-}
+// 通常推荐同一个 结构体采用相同的 receiver 类型
 
-// Rect类型的receiver
+// Perimeter Rect类型的receiver
 func (r Rect) Perimeter() int {
 	return 2 * (r.Width + r.Height)
+}
+
+// Area *Rect类型的receiver
+// 指针接收者可以减少接收者的拷贝消耗, 并且可以修改接收者
+func (r *Rect) Area() int {
+	return r.Width * r.Height
 }
