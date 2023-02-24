89c038ce74b31c9e4bfbef5407f4f8ba55e7099b
GodYao <liyaoo1995@gmail.com>
2023-02-24 18:31:05 +0800

    😀🌴⭐🌎☀️🌈🌽: [Go examples] language goroutines go

--- go.dev/goexamples/language/goroutines/go/main.go
+++ go.dev/goexamples/language/goroutines/go/main.go
@@ -10,20 +10,23 @@ func f(from string) {
 		fmt.Println(from, ":", i)
 	}
 }
+
 // 并发的方式运行g
 func main() {
-	// sync
+	// sync 同步执行
 	f("sync")
 
-	// async
+	// async 异步执行
+	// goroutine 是 轻量级的"线程"
 	go f("async")
-
 
 	// 匿名 async
 	go func(msg string) {
 		fmt.Println(msg)
 	}("Golang")
 
+	// 必须等到其执行, 更好的办法是采用 WaitGroup
+	// goroutines 之间是同时执行的 (单核并发、多核可能是并行的)
 	time.Sleep(1 * time.Second)
 	fmt.Println("Done")
 }
