253a121828b3182b6bd347369b53ca013ad5bba7
GodYao <liyaoo1995@gmail.com>
2023-02-25 00:27:41 +0800

    😀🌴⭐🌎☀️🌈🌽: [Go Web Programming] third-party routing

--- go.dev/webprogramming/routing/router.go
+++ go.dev/webprogramming/routing/router.go
@@ -1,4 +1,4 @@
-package http
+package main
 
 import (
 	"fmt"
@@ -7,8 +7,8 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func RouterServer() {
-	// thirdParty MUX
+func main() {
+	// 第三方的 routing 库
 	router := mux.NewRouter()
 
 	router.HandleFunc("/api/v1/{user}/{name}/", func(w http.ResponseWriter, r *http.Request) {
@@ -16,9 +16,10 @@ func RouterServer() {
 		fmt.Println(vars["user"], vars["name"])
 	})
 
+	// assets 文件服务
 	fs := http.FileServer(http.Dir("static/"))
 	// Need user New router to handle
 	router.Handle("/static/", http.StripPrefix("/static/", fs))
 
-	http.ListenAndServe(":8080", router)
+	_ = http.ListenAndServe(":8080", router)
 }
