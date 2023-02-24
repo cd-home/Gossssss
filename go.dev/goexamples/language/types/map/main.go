24c52c8874929e48db849e8819c82f7a17dd543e
GodYao <liyaoo1995@gmail.com>
2023-02-24 16:14:34 +0800

    😀🌴⭐🌎☀️🌈🌽: [Go examples] language types map

--- go.dev/goexamples/language/types/map/main.go
+++ go.dev/goexamples/language/types/map/main.go
@@ -3,23 +3,36 @@ package main
 import "fmt"
 
 func main() {
+	// 关联数据类型、或者称为映射、字典, 也是动态增长的
 	m := make(map[string]int)
+
+	// set 语法
 	m["a"] = 1
 	m["b"] = 2
 
 	fmt.Println(m)
 
+	// 返回key-value pair
 	fmt.Println(len(m))
 
+	// 获取key-value, 不存在也不会报错, 并且nil map是可读的
+	v := m["a"]
+	fmt.Println(v)
 
+	// 删除key
 	delete(m, "a")
 
 	fmt.Println(m)
 
+	// ok 模式 检测某个key 是否存在map 中
 	if v, ok := m["a"]; ok {
 		fmt.Println(v)
 	} else {
+		// 不存在返回value类型的零值
 		fmt.Println("No a")
 	}
 
+	// 字面量, 声明并且初始化
+	mps := map[string]string{"name": "yao", "age": "28"}
+	fmt.Println(mps)
 }
