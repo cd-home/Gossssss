349cd531f04609b0de6c41863290c132a11554b3
GodYao <liyaoo1995@gmail.com>
2023-02-26 02:38:12 +0800

    😀🌴⭐🌎☀️🌈🌽: [Go examples] language defer

--- go.dev/goexamples/language/controllflow/defer/main.go
+++ go.dev/goexamples/language/controllflow/defer/main.go
@@ -1,10 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	// 注册延迟函数，等待函数主体逻辑执行完成后执行
 	defer fmt.Println("Done")
-
 	fmt.Println("start")
+
+	fd, err := os.Open("name.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer fd.Close()
 }
