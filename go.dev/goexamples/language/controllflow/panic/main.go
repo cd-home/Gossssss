b03e616d5dc4f44b8c3caa717c0445839d3da3c5
GodYao <liyaoo1995@gmail.com>
2023-02-26 02:35:39 +0800

    😀🌴⭐🌎☀️🌈🌽: [Go examples] language panic

--- go.dev/goexamples/language/controllflow/panic/main.go
+++ go.dev/goexamples/language/controllflow/panic/main.go
@@ -1,6 +1,12 @@
 package main
 
+import "os"
+
 func main() {
 	// panic 的一种常见用法是：当函数返回我们不知道如何处理（或不想处理）的错误值时，中止操作
 	// 或者程序必要的组件出现问题，无法正常运行
+	_, err := os.Open("no exist")
+	if err != nil {
+		panic(err)
+	}
 }
