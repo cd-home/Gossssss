977e20ed9e22a7b50d468f846e3bf5dd4c8e84b5
GodYao <liyaoo1995@gmail.com>
2023-02-26 02:41:33 +0800

    😀🌴⭐🌎☀️🌈🌽: [Go examples] language recover

--- go.dev/goexamples/language/controllflow/recover/main.go
+++ go.dev/goexamples/language/controllflow/recover/main.go
@@ -1,1 +1,21 @@
-package recover
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	// 捕获panic
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println(r)
+		}
+	}()
+	_, err := os.Open("name.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("do not run this")
+}
