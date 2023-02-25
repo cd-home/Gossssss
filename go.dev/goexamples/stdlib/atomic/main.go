2b59a94d608ca8c819312b5cf6b7239c4f66a428
GodYao <liyaoo1995@gmail.com>
2023-02-26 02:11:19 +0800

    😀🌴⭐🌎☀️🌈🌽: [standardlibs] atomic

--- go.dev/goexamples/stdlib/atomic/main.go
+++ go.dev/goexamples/stdlib/atomic/main.go
@@ -7,7 +7,9 @@ import (
 )
 
 func main() {
+	// atomic 并发下修改某个值的状态
 	var ops uint64
+
 	var wg sync.WaitGroup
 
 	for i := 0; i < 50; i++ {
@@ -16,6 +18,7 @@ func main() {
 		go func() {
 			defer wg.Done()
 			for i := 0; i < 1000; i++ {
+				// 并发安全的, 提供低级别的原语来实现同步算法
 				atomic.AddUint64(&ops, 1)
 			}
 		}()
