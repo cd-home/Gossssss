5782f4c8a5ec532459897d3419250aeab45b2d29
GodYao <liyaoo1995@gmail.com>
2023-02-26 02:44:36 +0800

    😀🌴⭐🌎☀️🌈🌽: [Go examples] stdlib strings

--- go.dev/goexamples/stdlib/strings/main.go
+++ go.dev/goexamples/stdlib/strings/main.go
@@ -5,10 +5,12 @@ import (
 	"strings"
 )
 
-
 func main() {
 	s := "Hello World"
 	// 大量字符串处理函数都在strings包
 	fmt.Println(strings.HasPrefix(s, "He"))
 	fmt.Println(strings.Split(s, "o"))
+	fmt.Println(strings.Fields(s))
+	fmt.Println(strings.Cut("127.0.0.1:8080", ":"))
+	fmt.Println(strings.TrimPrefix(s, "H"))
 }
