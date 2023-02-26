a4676dbd9a67a4ff184a2ff843ad724fa04d0cec
GodYao <liyaoo1995@gmail.com>
2023-02-26 20:55:54 +0800

    😀🌴⭐🌎☀️🌈🌽: [Go examples] stdlib os open openfile

--- go.dev/goexamples/stdlib/os/file/main.go
+++ go.dev/goexamples/stdlib/os/file/main.go
@@ -2,30 +2,33 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"os"
 )
 
-func Check(e error)  {
+func Check(e error) {
 	if e != nil {
 		panic(e)
 	}
 }
 
 func main() {
-	path := "/Users/apple/Desktop/otherSpace/CodeExamples/Go by Example/46-URL.go"
+	pwd, _ := os.Getwd()
+	path := pwd + "/go.dev/goexamples/stdlib/os/file/main.go"
 	// 全部读取
-	data, err := ioutil.ReadFile(path)
+	data, err := os.ReadFile(path)
 	Check(err)
 	fmt.Println(string(data))
 
 	fmt.Println("==========================================")
 
-	// 文件具柄  更多的文件操作、读、写、信息、权限
+	// 文件具柄 只读打开
 	f, err := os.Open(path)
 	Check(err)
 	buf := make([]byte, 100)
 	n, err := f.Read(buf)
 	Check(err)
 	fmt.Println(string(buf[0:n]))
+
+	// 文件具柄  更多的文件操作、读、写、信息、权限 4 2 1
+	//os.OpenFile(path, os.O_CREATE|os.O_APPEND|os.O_RDWR, 0644)
 }
